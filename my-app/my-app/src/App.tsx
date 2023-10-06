3d7a548034d18fa7c0bc5331e0ff080b598ca2c0
Taha Rehman <taharehman@Tahas-MacBook-Air.local>
2023-10-06 23:46:37 +0500

    setting up

---
+++ my-app/my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { DatePicker } from "antd";
+
+function App() {
+  return (
+    <>
+      <DatePicker></DatePicker>
+    </>
+  );
+}
+
+export default App;
